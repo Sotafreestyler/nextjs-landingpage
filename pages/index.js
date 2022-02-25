ecbfd7fca455a36127fc0f51e9c78a234e2c1ab7
Sota Ito <>
2022-02-25 23:12:21 +0900

    文言修正

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          このサイトはNext.jsプロジェクトで作られています。{' '}
+          このサイトはNext.jsプロジェクトでそうたが作りました。{' '}
         </p>  
 
         <SliderImage src={`https://it-web-life.com/wp-content/uploads/2020/09/create_react_app.png`} />
